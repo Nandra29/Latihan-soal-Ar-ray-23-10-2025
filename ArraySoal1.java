a7c086e517d2c820a292ca57b246822bb9450e47
Nandra29 <mohammad_shihab_34rpl@student.smktelkom-mlg.sch.id>
2025-10-23 09:58:06 +0700

    Add files via upload

---
+++ ArraySoal1.java
@@ -0,0 +1,30 @@
+
+
+public class ArraySoal1 {
+    public static void main(String[] args) {
+        String Kota[] = {"Surabaya","Gondanglegi City","Gresik","Sidoarjo","Batu","Bandung","Gadang City Hardcore"};
+        System.err.println("Nama Kota");
+          for ( int i = 0; i < Kota.length; i++) {
+            System.out.println(i+1 + "."+Kota[i]);
+    }
+    
+    //soal No 2
+
+     System.out.println();
+     String MataPelajaran[] =  {"Matematika","Ipa","Ipas","Agama","Bahasa Indonesia","Akidah Akhlak","Senibudaya"};
+          System.err.println("Nama MataPelajaran");
+          for ( int i = 0; i < MataPelajaran.length; i++) {
+            System.out.println(i+1 + "."+MataPelajaran[i]);
+    
+}
+   // soal no 3
+      System.out.println();
+     String HewanPeliharaan[] =  {"Ikan","Kuda","Hamster","Kucing","Asep"};
+          System.err.println("Nama Hewan Peliharaan");
+          for ( int i = 0; i < HewanPeliharaan.length; i++) {
+            System.out.println(i+1 + "."+HewanPeliharaan[i]);
+       
+}
+
+}
+}
